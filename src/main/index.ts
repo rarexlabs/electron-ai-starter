6ad7475fbe89297a60e904c3d32001b2c335af5f
Roy Bao <roy@roybao.com>
2025-07-10 18:00:16 -0700

    starter template

--- src/main/index.ts
+++ src/main/index.ts
@@ -46,7 +46,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    title: 'WorthyFiles',
+    title: 'Electron Starter',
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
