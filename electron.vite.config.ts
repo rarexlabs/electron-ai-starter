95d4d2912b61175f092a7a574496081dee1dc5b2
Roy Bao <roy@roybao.com>
2025-07-15 18:09:10 -0700

    Fix building path

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,29 +1,44 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin, swcPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, swcPlugin, loadEnv } from 'electron-vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  main: {
-    resolve: {
-      alias: {
-        '@main': resolve('src/main'),
-        '@common': resolve('src/common'),
-        '@resources': resolve('resources')
-      }
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  const env = loadEnv(mode)
+  console.log(env)
+  const processEnv = Object.keys(env).reduce((acc, key) => {
+    acc[`process.env.${key}`] = JSON.stringify(env[key])
+    return acc
+  }, {})
+
+  return {
+    main: {
+      define: processEnv,
+      build: {
+        sourcemap: true,
+        minify: true
+      },
+      resolve: {
+        alias: {
+          '@main': resolve('src/main'),
+          '@common': resolve('src/common'),
+          '@resources': resolve('resources')
+        }
+      },
+      plugins: [externalizeDepsPlugin(), swcPlugin()]
     },
-    plugins: [externalizeDepsPlugin(), swcPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin(), swcPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src'),
-        '@common': resolve('src/common')
-      }
+    preload: {
+      plugins: [externalizeDepsPlugin(), swcPlugin()]
     },
-    plugins: [react(), tailwindcss()]
+    renderer: {
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src'),
+          '@common': resolve('src/common')
+        }
+      },
+      plugins: [react(), tailwindcss()]
+    }
   }
 })
